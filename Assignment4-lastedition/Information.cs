db0048c3f52c9ae4886a05b1259554f0e492ac09
eness <eness@LAPTOP-UATHR0DF>
2023-01-24 13:35:38 +0100

    Add project files.

---
+++ Assignment4-lastedition/Information.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment4_lastedition
+{
+    class Information
+    {
+        public static void Print()
+        {
+            Console.Clear();
+            Console.WriteLine("ENES YAS");
+            Console.WriteLine("\n" + "[Press Enter to return to main menu...]");
+            Console.ReadLine();
+        }
+    }
+}
