8815a96bae8bc9daffb504674d8f9588d1e803a0
darkprogr <119452385+darkprogr@users.noreply.github.com>
2023-01-24 13:36:30 +0100

    Update Information.cs

--- Assignment4-lastedition/Information.cs
+++ Assignment4-lastedition/Information.cs
@@ -7,7 +7,7 @@ namespace Assignment4_lastedition
         public static void Print()
         {
             Console.Clear();
-            Console.WriteLine("ENES YAS");
+            Console.WriteLine("ENES YASTI");
             Console.WriteLine("\n" + "[Press Enter to return to main menu...]");
             Console.ReadLine();
         }
